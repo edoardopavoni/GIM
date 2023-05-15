48455c5253b2898d14b122e26e76611ae2dbf430
edoardopavoni <edoardo.pavoni@student.supsi.ch>
2023-05-15 16:44:46 +0200

    update

---
+++ Esercizio_4/1_orologio/sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+	createCanvas(400, 400)
+}
+
+function draw(){
+	
+	let ora = hour() + ":" + minute () + ":" + second()
+	
+function draw(){
+
+}
+
+
+	background(200)
+	
+	textSize(40)
+	textFont("monospace")
+	textAlign (CENTER, CENTER)
+	fill (255,0,0)
+	text(ora, width/2, height/2)
+
+}
